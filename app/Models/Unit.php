0f88df1e21da72468fcddb838fab6551e8160bf8
fachiri <fachrycooles@gmail.com>
2023-08-26 00:08:30 +0800

    feat(Unit): add factory unit

--- app/Models/Unit.php
+++ app/Models/Unit.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Unit extends Model
 {
+  use HasFactory;
+
   protected $guarded = ['_token'];
 
   public function getRouteKeyName(): string
